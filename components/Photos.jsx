86bc61584f2e7d127c76f755dbf1d24dbfabb591
Tom Zmyslo <tom@zmyslo.com>
2020-09-21 23:19:29 -0400

    Removes unused API files and refactors the Photos Grid

--- components/Photos.jsx
+++ components/Photos.jsx
@@ -1,31 +1,22 @@
 import React from 'react';
 import Card from './Card';
+import photos from '../data/photos.json';
 
 function Photos() {
   return(
     <div className="container">
       <div className="row">
-        <div className="col-lg-4 mb-3">
-          <Card
-            source="images/glamour1-cropped.jpg"
-            title="Lorem ipsum dolor sit amet"
-            caption="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis risus sed vulputate odio ut."
-          />
-        </div>
-        <div className="col-lg-4 mb-3">
-          <Card
-            source="images/glamour2-cropped.jpg"
-            title="Kalamazoo Central Maroon Giants Hall of Fame Dinner"
-            caption="Jermaine Jackson Kalamazoo Public Schools School Board Trustee and Von Washington Jr. EXECUTIVE DIRECTOR Community relations Kalamazoo Promise"
-          />
-        </div>
-        <div className="col-lg-4 mb-3">
-          <Card
-            source="images/glamour3-cropped.jpg"
-            title="Lorem ipsum dolor sit amet"
-            caption="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis risus sed vulputate odio ut."
-          />
-        </div>
+        {photos.map((photo) => {
+          return(
+            <div key={photo.id} className="col-lg-4 mb-3">
+              <Card
+                source={photo.source}
+                title={photo.title}
+                caption={photo.caption}
+              />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
