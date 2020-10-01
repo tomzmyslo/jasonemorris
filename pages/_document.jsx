eeb6814685f53464d32f71e2def6f2d5477c4ba3
Tom Zmyslo <tom@zmyslo.com>
2020-10-01 01:32:38 -0400

    fixup! fixup! Update Facebook SDK code

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -7,24 +7,24 @@ class MyDocument extends Document {
       <Html>
         <Head />
         <body>
+          <div id="fb-root" />
           <script>
             window.fbAsyncInit = function() {
               FB.init({
-                appId: '767113464151084',
-                xfbml: true,
-                version: 'v8.0'
-              })
-              FB.AppEvents.logPageView();
+                appId            : 'your-app-id',
+                autoLogAppEvents : true,
+                xfbml            : true,
+                version          : 'v8.0'
+              }
             };
-
-            (function(d, s, id){
-              var js, fjs = d.getElementsByTagName(s)[0];
-              if (d.getElementById(id)) {return;}
-              js = d.createElement(s); js.id = id;
-              js.src = "https://connect.facebook.net/en_US/sdk.js";
-              fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));
           </script>
+          <script
+            async
+            defer
+            crossOrigin="anonymous"
+            src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v8.0"
+            nonce="jTvBIFTO"
+          />
           <Main />
           <NextScript />
         </body>
